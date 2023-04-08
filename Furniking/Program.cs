0ccd726b31f551cde6c55c3d49b9b48abebb5f86
DESKTOP-IBJNLFM\MikeIt <mixaylo.skv@gmail.com>
2023-04-08 22:33:48 +0300

    review

--- Furniking/Program.cs
+++ Furniking/Program.cs
@@ -31,12 +31,9 @@ namespace Furniking
 
             builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
             builder.Services.AddTransient<ICategoryService, CategoryService>();
-
-			//builder.Services.AddTransient<IReviewRepository, ReviewRepository>();
-			//builder.Services.AddTransient<IReviewService, ReviewService>();
-
-			builder.Services.AddAutoMapper(typeof(CategoryProfile));
-			//builder.Services.AddAutoMapper(typeof(ReviewProfile));
+			builder.Services.AddTransient<IReviewRepository, ReviewRepository>();
+			builder.Services.AddTransient<IReviewService, ReviewService>();
+			builder.Services.AddAutoMapper(typeof(ReviewProfile));
 
 			// Add services to the container.
 			builder.Services.AddControllers();
