b21b26fbd712e03196b7e2e6a2da211b8622a109
Artur358 <itgamevenpc@gmail.com>
2023-04-13 01:40:16 +0300

    edit Furniture entity

--- Furniking.DAL/Data/Migrations/20230412223731_mainImage.Designer.cs
+++ Furniking.DAL/Data/Migrations/20230412223731_mainImage.Designer.cs
@@ -4,6 +4,7 @@ using Furniking.DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Furniking.DAL.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230412223731_mainImage")]
+    partial class mainImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Furniking.DAL/Entities/Furniture.cs
+++ Furniking.DAL/Entities/Furniture.cs
@@ -6,6 +6,8 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
 
+        public int? MainImageId { get; set; } 
+
         public virtual ICollection<Image> Images { get; set; }
 
         public int CategoryId { get; set; }
